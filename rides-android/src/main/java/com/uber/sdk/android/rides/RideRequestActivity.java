74af3f9b16dfc0ff67800ebdfe865c7580046ac5
Edbert Chan <edbert@uber.com>
2021-08-12 14:45:19 -0700

    Update RideRequestActivity.java
    Retry

--- rides-android/src/main/java/com/uber/sdk/android/rides/RideRequestActivity.java
+++ rides-android/src/main/java/com/uber/sdk/android/rides/RideRequestActivity.java
@@ -30,6 +30,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
