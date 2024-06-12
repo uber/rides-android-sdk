52f2ef93d5487491a492bf65a5f1e1e954e66f8e
lalwani <lalwani@uber.com>
2024-06-12 14:32:38 -0700

    upgraded libs and modified UberAuthButton to wrap content and customize shape

--- authentication/src/main/kotlin/com/uber/sdk2/auth/ui/UberAuthButton.kt
+++ authentication/src/main/kotlin/com/uber/sdk2/auth/ui/UberAuthButton.kt
@@ -1,21 +1,23 @@
 package com.uber.sdk2.auth.ui
 
-import android.media.Image
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -25,49 +27,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uber.sdk2.core.R
 
-/** A button that looks like the Uber button. */
-@Composable
-fun UberCustomButton(
-    text: String,
-    isWhite: Boolean = false,
-    onClick: () -> Unit
-) {
-    val interactionSource = remember { MutableInteractionSource() }
-    val isPressed = interactionSource.collectIsPressedAsState().value
-    val backgroundColor = if (isPressed) {
-        if (isWhite) colorResource(id = R.color.uber_white_40) else colorResource(id = R.color.uber_black_90)
-    } else {
-        if (isWhite) colorResource(id = R.color.uber_white) else colorResource(id = R.color.uber_black)
-    }
-
-    val textColor = if (isWhite) {
-        colorResource(id = R.color.uber_black)
-    } else {
-        colorResource(id = R.color.uber_white)
-    }
-
-    Text(
-        text = text,
-        color = textColor,
-        style = TextStyle(
-            fontSize = dimensionResource(id = R.dimen.ub__text_size).value.sp
-        ),
-        modifier = Modifier
-            .clip(RoundedCornerShape(4.dp))
-            .background(backgroundColor)
-            .clickable(interactionSource = interactionSource, indication = null, onClick = onClick)
-            .padding(16.dp)
-    )
-}
-
 @Composable
 fun UberAuthButton(
     text: String,
     isWhite: Boolean = false,
+    shape: Shape = RoundedCornerShape(4.dp),
     onClick: () -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed = interactionSource.collectIsPressedAsState().value
+
     val backgroundColor = if (isPressed) {
         if (isWhite) colorResource(id = R.color.uber_white_40) else colorResource(id = R.color.uber_black_90)
     } else {
@@ -80,43 +49,27 @@ fun UberAuthButton(
         colorResource(id = R.color.uber_white)
     }
 
-    val logo = if (isWhite) {
-        painterResource(id = R.drawable.uber_logotype_black)
-    } else {
-        painterResource(id = R.drawable.uber_logotype_white)
-    }
+    val iconResId = if (isWhite) R.drawable.uber_logotype_black else R.drawable.uber_logotype_white
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Button(
+        onClick = onClick,
         modifier = Modifier
-            .clip(RoundedCornerShape(4.dp))
-            .background(backgroundColor)
-            .clickable(interactionSource = interactionSource, indication = null, onClick = onClick)
-            .padding(dimensionResource(id = R.dimen.ub__standard_padding))
+            .wrapContentSize(),
+        colors = ButtonDefaults.buttonColors(containerColor = backgroundColor, contentColor = textColor),
+        shape = RoundedCornerShape(4.dp)
     ) {
-        Image(
-            painter = logo,
+        Icon(
+            painter = painterResource(id = iconResId),
             contentDescription = null,
             modifier = Modifier.padding(end = dimensionResource(id = com.uber.sdk2.auth.R.dimen.ub__signin_margin))
         )
         Text(
-            text = text,
+            text = text.uppercase(),
             color = textColor,
-            style = TextStyle(
-                fontSize = dimensionResource(id = R.dimen.ub__secondary_text_size).value.sp
-            ),
-            modifier = Modifier.weight(1f)
+            style = TextStyle(fontSize = dimensionResource(id = R.dimen.ub__secondary_text_size).value.sp),
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.ub__standard_padding))
+                .wrapContentWidth()
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun UberButtonPreview() {
-    Column {
-        UberCustomButton(text = "Default Uber Button", onClick = { /* Do something */ })
-        UberCustomButton(text = "White Uber Button", isWhite = true, onClick = { /* Do something */ })
-        UberAuthButton(text = "Sign in with Uber", onClick = { /* Do something */ })
-        UberAuthButton(text = "Sign in with Uber (White)", isWhite = true, onClick = { /* Do something */ })
     }
 }
